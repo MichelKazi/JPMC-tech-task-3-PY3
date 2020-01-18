0a37e936d6217c30822907062b45bff4d971e4b3
Michelkazi <michel.m.kazi@gmail.com>
2020-01-18 10:29:07 -0500

    fixed error in schema that prevented graph from rendering properly

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -7,7 +7,7 @@ export interface Row {
   timestamp: Date,
 	upper_bound: number,
 	lower_bound: number,
-	trigger_alert: number | null, 
+	trigger_alert: number | undefined, 
 }
 
 
@@ -17,18 +17,16 @@ export class DataManipulator {
 		const priceDEF = (serverResponds[1].top_ask.price + serverResponds[1].top_bid.price) / 2;
 		const ratio = priceABC/priceDEF;
 		const upperBound = 1.05;
-		const lowerBound = 1.05;
-		const timestamp = serverResponds[0].timestamp > serverResponds[1].timestamp ? 
-			serverResponds[0].timestamp : serverResponds[1].timestamp;
+		const lowerBound = .95;
       return {
-				price_abc :priceABC,
+				price_abc: priceABC,
 				price_def: priceDEF,
 				ratio, 
-				timestamp,
+				timestamp: serverResponds[0].timestamp > serverResponds[1].timestamp ? 
+					serverResponds[0].timestamp : serverResponds[1].timestamp,
 				upper_bound: upperBound,
 				lower_bound: lowerBound,
-				trigger_alert: (ratio > upperBound || ratio < lowerBound) ? ratio : null
-				
+				trigger_alert: (ratio > upperBound || ratio < lowerBound) ? ratio : undefined
       };
   }
 }
